d71c270ee9ce92b975bd1a8cbc88ddd58eae5cc7
Unknown <marc.destefano@gmail.com>
2018-09-19 21:24:40 -0400

    Sending POST command to Stellarium via legacy WWW post method. New one just doesn't work

---
+++ Assets/Scripts/StellariumInterface.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class StellariumInterface : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine("Upload");
+        }
+	}
+
+    IEnumerator Upload()
+    {
+       
+        WWWForm form = new WWWForm();
+        form.AddField("fov", "90");
+
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost:8090/api/main/fov", form);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+            
+        
+    }
+
+}
